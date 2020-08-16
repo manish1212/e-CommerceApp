04c7fe36997ced94004d1ec76f87058bf1f3065b
Manish <mp121995@gmail.com>
2020-08-16 15:11:28 -0700

    installed react app

---
+++ frontend/src/data.js
@@ -0,0 +1,24 @@
+export default  {
+    products:[{
+        name:'Slim Shirt',
+        image:'/images/d1.jpg',
+        price:'$60',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        name:'Fit Shirt',
+        image:'/images/d1.jpg',
+        price:'$60',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        name:'Best Shirt',
+        image:'/images/d1.jpg',
+        price:'$60',
+        rating:4.5,
+        numReviews:10
+    }
+]
+}
